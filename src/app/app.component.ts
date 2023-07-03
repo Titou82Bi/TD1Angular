399fe68fd31a598c6d6ccde7db715791c83745e4
Titou82Bi <inacio.baptiste@gmail.com>
2023-07-03 09:05:37 +0200

    modification du composant app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD1Ex2';
+  imgURL="https://w0.peakpx.com/wallpaper/747/327/HD-wallpaper-tanjiro-pic-fan-art-anime-demon-slayer-kimetsu-no-yaiba.jpg"
+
+  nomClass="rouge";
+  isActif="true";
+  onClick(){
+    console.log("Boutton cliqué")
+  }
+
+  isDetail:boolean=false;
+  afficher(){
+    this.isDetail=true;
+  }
+  cacher(){
+    this.isDetail=false;
+  }
 }
